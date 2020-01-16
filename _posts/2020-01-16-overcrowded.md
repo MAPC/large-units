cbd19eabcab3a626e187e111856afdf1b5b2d7cd
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-01-16 16:23:04 -0500

    Set up project and bootstrap one chart
    * Add SASS compiling
    * Link in d3
    * Strip Jekyll template to bare-bones

---
+++ _posts/2020-01-16-overcrowded.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "Percent Overcrowded Households"
+description: "Click to view data visualization"
+---
+<h2 class="graph__title">Percent Overcrowded Households, Inner Core PUMAS, 2012-2016</h2>
+<svg class="overcrowded-frame"></svg>
+<script src="{{ 'assets/javascripts/overcrowded.js' | absolute_url }}" type="module"></script>
---
+++ assets/javascripts/overcrowded.js
@@ -0,0 +1,124 @@
+d3.csv("/assets/data/overcrowded.csv").then((data) => {
+  const margin = {top: 50, right: 75, bottom: 50, left: 75}
+  , width = 700 - margin.left - margin.right
+  , height = 420 - margin.top - margin.bottom;
+
+  const svg = d3.select(".overcrowded-frame")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+
+  const graph = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("class", "graph")
+    .attr("height", height+margin.top+margin.bottom)
+
+  const xScale = d3.scaleBand()
+    .range([0, width-margin.right])
+    .domain(data.map((d) => d.household ))
+    .round(true)
+    .padding(.65);
+
+  const yScale = d3.scaleLinear()
+    .range([height-25, 0])
+    .domain([0, .14]);
+
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3.axisLeft(yScale)
+    .ticks(8, "~%")
+
+  graph.append("g")
+  .call(xAxis)
+  .attr("transform", "translate(0," + (height - 25) + ")")
+
+
+  graph.selectAll("text")
+  .attr("class", "xaxis__label")
+  .call(wrap, xScale.bandwidth()+40)
+  graph.append("g")
+  .call(yAxis)
+
+  graph.selectAll("bar")
+    .data(data)
+    .enter().append("rect")
+    .style("fill", "steelblue")
+    .attr("x", function(d) { return xScale(d.household); })
+    .attr("width", xScale.bandwidth())
+    .attr("y", function(d) { return yScale(d.percentage) - 25; })
+    .attr("height", function(d) { return height - yScale(d.percentage); })
+    .on("mousemove", function(d) {
+      tooltip.html(displayToolTip(d))
+      tooltip.attr("width", "200")
+      tooltip.attr("height", "200")
+      tooltip.style("display", null)
+      .style("left", tooltipLeft(d3.event, document.querySelector('.tooltip')))
+      .style("top", tooltipTop(d3.event,  document.querySelector('.tooltip')))
+    })
+    .on("mouseleave", function(d) { tooltip.style("display", "none") });
+
+    const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("display", "none");
+        
+    tooltip.append("rect")
+    .attr("width", 50)
+    .attr("height", 50)
+    .attr("fill", "white")
+    .style("opacity", 0.5);
+
+    tooltip.append("text")
+    .attr("x", 15)
+    .attr("dy", "1.2em")
+    .style("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold");
+})
+
+function tooltipLeft(event, tooltip) {
+  if (event.pageX > 350) {
+    return event.pageX - tooltip.offsetWidth - 10 + "px"
+  } else {
+    return event.pageX + 10 + "px"
+  }
+}
+
+function tooltipTop(event, tooltip) {
+  if (event.pageY > 275) {
+    return event.pageY - tooltip.offsetHeight - 10 + "px"
+  } else {
+    return event.pageY + 10 + "px"
+  }
+}
+
+function displayToolTip(data){
+  return "<span class='tooltip__title'>" + data.household + "</span>"
+  + "<br/>" + d3.format("~%")(data.percentage) + " households"
+}
+
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
+function type(d) {
+  d.value = +d.value;
+  return d;
+}
