9e0fa159eab1e58b68d00b8e739f6daf4706ba05
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-02-03 11:37:06 -0500

    Update legend heights and add study area boundary
    * The new breakpoints are accordingly updated in both DataCommon and DigitalHub
    * Decouple map title and legend from MapBox controller divs, thus fixing an error on Safari wherein the legend only displayed on click
     * Fix several SASS linting edits
    
    .

--- _posts/2020-01-29-map-with-boundary.md
+++ _posts/2020-01-29-map-with-boundary.md
@@ -4,5 +4,26 @@ title:  "Region map with study area boundary"
 description: "Click to view data visualization"
 ---
 <div id="map" class="map__container"></div>
-<div class="map__legend--mobile"></div>
+<div class="map__overlay map__title" id="title">Family Sized Housing Units</div>
+<div class="map__overlay map__legend" id="legend">
+  <svg height='168' width='160'>
+    <rect width='16' height='16' x='8' y='8' style='fill:#0097c4; stroke: black; stroke-width: 1px;'></rect>
+    <text x='32' y='20' class='legend__label'>0%-25%</text>
+    <rect width='16' height='16' x='8' y='32' style='fill:#3b66b0; stroke: black; stroke-width: 1px;'></rect>
+    <text x='32' y='44' class='legend__label'>26%-50%</text>
+    <rect width='16' height='16' x='8' y='56' style='fill:#233069; stroke: black; stroke-width: 1px;'></rect>
+    <text x='32' y='70' class='legend__label'>51%-75%</text>
+    <rect width='16' height='16' x='8' y='80' style='fill:#111436; stroke: black; stroke-width: 1px;'></rect>
+    <text x='32' y='92' class='legend__label'>76%-100%</text>
+    <rect width='16' height='16' x='8' y='104' style='fill:#B57F00; stroke: black; stroke-width: 1px;'></rect>
+    <text x='32' y='116' class='legend__label'>Data unavailable</text>
+    <rect width='16' height='16' x='8' y='128' style='fill:black; stroke: black; stroke-width: 1px;'></rect>
+    <line x1='15' y1='144' x2='24' y2='135' style='stroke: #CFCECC;'></line>
+    <line x1='8' y1='144' x2='24' y2='128' style='stroke: #CFCECC;'></line>
+    <line x1='8' y1='137' x2='17' y2='128' style='stroke: #CFCECC;'></line>
+    <text x='32' y='140' class='legend__label'>High margin of error</text>
+    <line x1 ='8' y1='160' x2='24' y2='160' style='stroke: #7d7d7d; stroke-width: 4;'></line>
+    <text x='32' y='164' class='legend__label'>Study area boundary</text>
+  </svg>
+</div>
 <script src="{{'assets/javascripts/map-with-boundary.js' | absolute_url }}" type="module"></script>
--- assets/javascripts/map-with-boundary.js
+++ assets/javascripts/map-with-boundary.js
@@ -26,7 +26,6 @@ d3.csv('/large-units/assets/data/b25041_bedrooms_per_unit_ct.csv')
 
   map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
   map.on('load', () => {
-    addLegend(colorPalette);
     const colorPolygon = d3.scaleQuantize()
       .domain([0,1])
       .range(colorPalette);
@@ -78,45 +77,3 @@ d3.csv('/large-units/assets/data/b25041_bedrooms_per_unit_ct.csv')
       .addTo(map);
   });
 });
-
-function addLegend(colorPalette) {
-  document.querySelector('.mapboxgl-ctrl-top-left').innerHTML = "<h2 class='map__title'>Family Sized Housing Units</h2>";
-
-  document.querySelector('.mapboxgl-ctrl-top-right').innerHTML = "<aside class='map__legend'>"
-    + "<svg height='146' width='160'>"
-    + `<rect width='16' height='16' x='8' y='8' style='fill:${colorPalette[0]}; stroke: black; stroke-width: 1px;'></rect>`
-    + "<text x='32' y='20' class='legend__label'>0%-25%</text>"
-    + `<rect width='16' height='16' x='8' y='32' style='fill:${colorPalette[1]}; stroke: black; stroke-width: 1px;'></rect>`
-    + "<text x='32' y='44' class='legend__label'>26%-50%</text>"
-    + `<rect width='16' height='16' x='8' y='56' style='fill:${colorPalette[2]}; stroke: black; stroke-width: 1px;'></rect>`
-    + "<text x='32' y='70' class='legend__label'>51%-75%</text>"
-    + `<rect width='16' height='16' x='8' y='80' style='fill:${colorPalette[3]}; stroke: black; stroke-width: 1px;'></rect>`
-    + "<text x='32' y='92' class='legend__label'>76%-100%</text>"
-    + "<rect width='16' height='16' x='8' y='104' style='fill:#B57F00; stroke: black; stroke-width: 1px;'></rect>"
-    + "<text x='32' y='116' class='legend__label'>Data unavailable</text>"
-    + "<rect width='16' height='16' x='8' y='128' style='fill:black; stroke: black; stroke-width: 1px;'></rect>"
-    + "<line x1='15' y1='144' x2='24' y2='135' style='stroke: #CFCECC;'></line>"
-    + "<line x1='8' y1='144' x2='24' y2='128' style='stroke: #CFCECC;'></line>"
-    + "<line x1='8' y1='137' x2='17' y2='128' style='stroke: #CFCECC;'></line>"
-    + "<text x='32' y='140' class='legend__label'>High margin of error</text>"
-    + '</svg>'
-    + '</aside>';
-
-    document.querySelector('.map__legend--mobile').innerHTML = "<svg height='146' width='160'>"
-    + `<rect width='16' height='16' x='8' y='8' style='fill:${colorPalette[0]}; stroke: black; stroke-width: 1px;'></rect>`
-    + "<text x='32' y='20' class='legend__label'>0%-25%</text>"
-    + `<rect width='16' height='16' x='8' y='32' style='fill:${colorPalette[1]}; stroke: black; stroke-width: 1px;'></rect>`
-    + "<text x='32' y='44' class='legend__label'>26%-50%</text>"
-    + `<rect width='16' height='16' x='8' y='56' style='fill:${colorPalette[2]}; stroke: black; stroke-width: 1px;'></rect>`
-    + "<text x='32' y='70' class='legend__label'>51%-75%</text>"
-    + `<rect width='16' height='16' x='8' y='80' style='fill:${colorPalette[3]}; stroke: black; stroke-width: 1px;'></rect>`
-    + "<text x='32' y='92' class='legend__label'>76%-100%</text>"
-    + "<rect width='16' height='16' x='8' y='104' style='fill:#B57F00; stroke: black; stroke-width: 1px;'></rect>"
-    + "<text x='32' y='116' class='legend__label'>Data unavailable</text>"
-    + "<rect width='16' height='16' x='8' y='128' style='fill:black; stroke: black; stroke-width: 1px;'></rect>"
-    + "<line x1='15' y1='144' x2='24' y2='135' style='stroke: #CFCECC;'></line>"
-    + "<line x1='8' y1='144' x2='24' y2='128' style='stroke: #CFCECC;'></line>"
-    + "<line x1='8' y1='137' x2='17' y2='128' style='stroke: #CFCECC;'></line>"
-    + "<text x='32' y='140' class='legend__label'>High margin of error</text>"
-    + '</svg>'  
-}
